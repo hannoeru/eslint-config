2836cc9311b6fcf013c464e2491003dd0c6edd9b
hanlee <me@hanlee.co>
2021-11-22 19:45:17 +0900

    feat: disable vue/multi-word-component-names

--- packages/vue-js/index.js
+++ packages/vue-js/index.js
@@ -21,5 +21,6 @@ module.exports = {
     // 'vue/html-self-closing': 'off',
     'vue/no-v-html': 'off',
     'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 }
--- packages/vue/index.js
+++ packages/vue/index.js
@@ -22,5 +22,6 @@ module.exports = {
     // 'vue/html-self-closing': 'off',
     'vue/no-v-html': 'off',
     'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
   },
 }
