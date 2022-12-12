f79f9d7ddb7f5977626bb264467e5a71e144cfaf
hanlee <me@hanlee.co>
2022-12-13 00:32:33 +0900

    chore: fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,5 +1,4 @@
 import { execSync } from 'child_process'
-import fs from 'fs-extra'
 import { $ } from 'zx'
 
 await $`pnpm changelog`
@@ -7,8 +6,3 @@ await $`pnpm changelog`
 execSync('pnpm dlx bumpp package.json packages/*/package.json --tag --commit', { stdio: 'inherit' })
 
 await $`pnpm -r publish --access public --no-git-checks`
-
-const { version } = await fs.readJSON('package.json')
-
-await $`git push`
-await $`git push origin v${version}`
